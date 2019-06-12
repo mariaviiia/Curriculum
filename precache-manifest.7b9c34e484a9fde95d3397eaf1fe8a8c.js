a7205d3092deba5211c977d530d6153ae46b678c
maria ochoa diez <mariaviiia@hotmail.com>
2019-06-12 10:10:56 -0500

    Updates

---
+++ precache-manifest.7b9c34e484a9fde95d3397eaf1fe8a8c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2284e05ed131d6873292b427f17cdb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19eb8db83a70c20bcdbc",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "31cb37892ec0639c8ada",
+    "url": "/static/css/main.e704ed84.chunk.css"
+  },
+  {
+    "revision": "19eb8db83a70c20bcdbc",
+    "url": "/static/js/2.dd037799.chunk.js"
+  },
+  {
+    "revision": "31cb37892ec0639c8ada",
+    "url": "/static/js/main.93a5d8a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3db595a5c9a06d39a8f6a0e4a88d9e70",
+    "url": "/static/media/LOGO.3db595a5.svg"
+  },
+  {
+    "revision": "eee0b154e0326329b011aea3120c7835",
+    "url": "/static/media/bird.eee0b154.svg"
+  },
+  {
+    "revision": "0b29b20d8ca2a55734824ba4c016c8c1",
+    "url": "/static/media/dv.0b29b20d.png"
+  },
+  {
+    "revision": "7fba9d50ac22754b382fd141c217ef8e",
+    "url": "/static/media/fe.7fba9d50.png"
+  },
+  {
+    "revision": "ac0e3f0424fcd5187cf571d36ccdd43b",
+    "url": "/static/media/js.ac0e3f04.png"
+  },
+  {
+    "revision": "942312351d8816742f8f754e0ae50890",
+    "url": "/static/media/rw.94231235.png"
+  }
+]);
